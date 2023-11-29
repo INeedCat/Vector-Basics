13883ccdc6f61136856c64dc31d624a859185217
INeedCat <97716183+INeedCat@users.noreply.github.com>
2023-11-29 14:11:50 -0700

    Update script.js
    added Normalize(), Add(), and Sub() methods

--- script.js
+++ script.js
@@ -5,15 +5,28 @@ canvas.width = 1200;
 canvas.height = 600;
 
 class Vector{
-    constructor(mag=1, x=0, y=0){
+    constructor(x=0, y=0){
         this.theta = Math.atan(y/x);
         this.x = x;
         this.y = y;
-        if(x > 0 && y > 0){
-            this.mag = Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
-        } else {
-            this.x = Math.cos(this.theta) * this.mag;
-            this.y = Math.sin(this.theta) * this.mag;
-        }
+        this.mag = Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+    }
+
+    normalize(){
+        let nx = Math.cos(this.theta);
+        let ny = Math.sin(this.theta);
+        return new Vector(nx, ny);
+    }
+
+    add(vec){
+        let ax = this.x + vec.x;
+        let ay = this.y + vec.y;
+        return new Vector(ax, ay);
+    }
+
+    sub(vec){
+        let sx = this.x - vec.x;
+        let sy = this.y - vec.y;
+        return new Vector(sx, sy);
     }
 }  
